a3bbd82d84f091908adb12ba04d5372551452811
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-06-04 20:08:52 +0300

    del comments

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,6 @@ export const App = () => {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="tweets" element={<Tweets />} />
-
-          {/* <Route path="movies" element={<Movies />} />
-
-          <Route path="/movies/:movieId" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route> */}
         </Route>
       </Routes>
     </Suspense>
