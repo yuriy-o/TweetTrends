89c73ac96a65d5263ad67b51d16ba2e33d4ab7c3
Yuriy <82809592+yuriy-o@users.noreply.github.com>
2023-06-04 20:54:46 +0300

    chg url

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export const getTweets = async () => {
   try {
-    const response = await axios.get(process.env.REACT_APP_BASE_URL);
+    const response = await axios.get(
+      'https://6478726a362560649a2dc983.mockapi.io/api/v1/users'
+    );
 
     return response.data;
   } catch (error) {
